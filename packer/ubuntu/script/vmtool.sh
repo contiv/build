82d3e0a9d583366320fd600db1f1ac41f61f25b0
Erik Hollensbe <github@hollensbe.org>
2015-08-11 15:46:29 -0700

    packer: atlas uploading support
    Signed-off-by: Erik Hollensbe <github@hollensbe.org>

--- packer/ubuntu/script/vmtool.sh
+++ packer/ubuntu/script/vmtool.sh
@@ -59,7 +59,7 @@ if [[ $PACKER_BUILDER_TYPE =~ virtualbox ]]; then
 
     VBOX_VERSION=$(cat /home/${SSH_USER}/.vbox_version)
     mount -o loop /home/${SSH_USER}/VBoxGuestAdditions_$VBOX_VERSION.iso /mnt
-    /mnt/VBoxLinuxAdditions.run
+    /mnt/VBoxLinuxAdditions.run --nox11
     umount /mnt
     rm /home/${SSH_USER}/VBoxGuestAdditions_$VBOX_VERSION.iso
     rm /home/${SSH_USER}/.vbox_version
