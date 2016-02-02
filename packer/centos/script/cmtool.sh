f13410e6f7a60177bc054e9a2b5377295503945a
Madhav Puri <madhav.puri@gmail.com>
2016-02-01 22:16:54 -0800

    use apt or yum instead of pip for installing ansible
    I was seeing issue where pip was still downloading old ansible
    core module resulting in ansible errors.
    
    Also pip messes up with pyhton packages already installed by os
    specific package managers resulting in errors later during yum/apt
    updates, so it seems better to avoid it as we use yum/apt a lot for
    package management.
    
    Signed-off-by: Madhav Puri <madhav.puri@gmail.com>

--- packer/centos/script/cmtool.sh
+++ packer/centos/script/cmtool.sh
@@ -1,3 +1,3 @@
 yum install epel-release -y
-yum install python-devel gcc gcc-c++ make openssl-devel python-pip -y
-pip install ansible
+yum install python-devel gcc gcc-c++ make openssl-devel -y
+yum install -y ansible
