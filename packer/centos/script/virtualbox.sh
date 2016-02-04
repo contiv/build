58d13f8a8a819bafc6f0bb98806adb0c269e45d0
Erik Hollensbe <github@hollensbe.org>
2016-02-04 08:00:53 -0800

    packer/centos: Tooling to fix build for volplugin
    Signed-off-by: Erik Hollensbe <github@hollensbe.org>

--- packer/centos/script/virtualbox.sh
+++ packer/centos/script/virtualbox.sh
@@ -6,14 +6,14 @@ SSH_USER_HOME=${SSH_USER_HOME:-/home/${SSH_USER}}
 if [[ $PACKER_BUILDER_TYPE =~ virtualbox ]]; then
     echo "==> Installing VirtualBox guest additions"
     # Assume that we've installed all the prerequisites:
-    # kernel-headers-$(uname -r) kernel-devel-$(uname -r) gcc make perl
+    yum install -y kernel-headers-$(uname -r) kernel-devel-$(uname -r) gcc make perl
     # from the install media via ks.cfg
 
     VBOX_VERSION=$(cat $SSH_USER_HOME/.vbox_version)
-    mount -o loop $SSH_USER_HOME/VBoxGuestAdditions_$VBOX_VERSION.iso /mnt
+    mount -o loop $SSH_USER_HOME/VBoxGuestAdditions.iso /mnt
     sh /mnt/VBoxLinuxAdditions.run --nox11
     umount /mnt
-    rm -rf $SSH_USER_HOME/VBoxGuestAdditions_$VBOX_VERSION.iso
+    rm -rf $SSH_USER_HOME/VBoxGuestAdditions.iso
     rm -f $SSH_USER_HOME/.vbox_version
 
     if [[ $VBOX_VERSION = "4.3.10" ]]; then
